ab9f241da7b8f790e9593a983e8229078c3d47e0
WojtekSasiela <wojtek.sasiela@gmail.com>
2015-01-23 10:22:00 +0100

    working fixtures: pracownicy

--- src/AppBundle/DataFixtures/ORM/LoadPracownicy.php
+++ src/AppBundle/DataFixtures/ORM/LoadPracownicy.php
@@ -5,7 +5,7 @@ namespace Gajdaw\BDDTutorial\GeographyBundle\DataFixtures\ORM;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Yaml\Yaml;
-use Gajdaw\BDDTutorial\AppBundle\Entity\Pracownik;
+use AppBundle\Entity\Pracownik;
 
 class LoadPracownicy implements FixtureInterface
 {
@@ -20,7 +20,7 @@ class LoadPracownicy implements FixtureInterface
         foreach ($yml as $item) {
             $pracownik = new Pracownik();
             $pracownik->setImie($item['imie']);
-            $pracownik->setnaziwsko($item['nazwisko']);
+            $pracownik->setNazwisko($item['nazwisko']);
             $manager->persist($pracownik);
         }
 
